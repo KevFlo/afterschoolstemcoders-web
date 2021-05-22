24b33a700dde803670fce5beb20c37c11180346e
Kevin <floreskevin51398@gmail.com>
2021-05-22 14:48:38 -0500

    added some css and styling to header, logo and icon -Kevin Flores

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,21 @@ const Home = () => {
     <>
       <Head>
         <title>After-School STEM Coders</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/SCSPrototype.png "/>
+        <header className={"headerClass"}>
+            <img className={"imageClass"} src={"SCSPrototype.png"} alt={"org logo"} ></img>
+            <link rel="stylesheet" href="styles.css" type="text/css" charSet="utf-8"/>
+            <style type={"text/css"}></style>
+
+            <button className={"button button2"} type={"text/css"}>[1]Page</button>
+
+
+
+
+        </header>
       </Head>
       <div className="h-screen bg-gray-500 flex justify-center items-center">
-        <div className="text-3xl text-center">Coming Soon!</div>
+        <div className="text-3xl text-center">Coming Soon! UWU </div>
       </div>
 
       <footer></footer>
